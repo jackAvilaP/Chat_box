474790be96961d22c7f91e366b6d101fd5c5cdbc
jacksonPeaku <jackson.avila@peaku.ai>
2023-02-26 20:15:33 -0500

    add server and socket.io, socket.io-client

--- chatBox/src/App.jsx
+++ chatBox/src/App.jsx
@@ -1,8 +1,15 @@
+// https://www.figma.com/file/RkPV21ng8Npkh2MJdTPoat/Direct-messaging-app-(Community)?node-id=0%3A1&t=Qw3OXh8Qn1tU2uz5-0
+
 import MainList from "./components/List/MainList";
 import Chat from "./components/mainChat/Chat";
 import NavBar from "./components/Nav/NavBar";
 
-// https://www.figma.com/file/RkPV21ng8Npkh2MJdTPoat/Direct-messaging-app-(Community)?node-id=0%3A1&t=Qw3OXh8Qn1tU2uz5-0
+//import socket.io-client
+import io from 'socket.io-client';
+
+//object return socket
+const socket = io('http://localhost:4000')
+
 
 function App() {
   return (
@@ -13,7 +20,7 @@ function App() {
       </section>
 
       {/* Section list chats, search and people */}
-      <section  className="ml-9">
+      <section className="ml-9">
         <MainList />
       </section>
 
---
+++ server/index.js
@@ -0,0 +1,32 @@
+//React Socketio Chat - Paso a Paso (Despliegue, Tailwindcss, Nodejs)
+
+import express from 'express';
+import morgan from 'morgan';
+import { Server as SocketServer } from 'socket.io';
+import http from 'http';
+import cors from 'cors';
+
+import { PORT } from './config.js';
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+});
+
+app.use(cors());
+app.use(morgan('dev'));
+
+//listen event new connect
+io.on('connection', (socket) => {
+    //id de cada pestaña de que habra
+    console.log(socket.id)
+    console.log("a user connected")
+})
+
+server.listen(PORT);
+console.log('Server started on port ', PORT) 
